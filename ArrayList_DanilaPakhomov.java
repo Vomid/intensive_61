b08036d39f37ac69fbc0abb3be3b947788e6bedf
Пахомов Данила <VomidHere@bnkr.com>
2024-04-18 17:45:28 +0300

    Первый коммит
    Создан класс ArrayList, настроен .gitignore

---
+++ ArrayList_DanilaPakhomov.java
@@ -0,0 +1,15 @@
+
+public class ArrayList_DanilaPakhomov {
+    public interface IntensiveList<E> {
+        int size();
+            void add(E element);
+            void add(int index, E element);
+            E get (int index);
+            E set (int index, E element);
+            E remove(int index);
+            void clear();
+        void quickSort(Comparator<E> comparator);
+        boolean isSorted();
+        void split(int size);
+    }
+}
